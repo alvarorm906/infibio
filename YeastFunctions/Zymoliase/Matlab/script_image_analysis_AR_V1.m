e3d03cc644e402dd950be3e42002ee37f8d74e46
Álvaro <122318029+alvarorm906@users.noreply.github.com>
2024-03-08 12:50:40 +0100

    Update script_image_analysis_AR_V1.m

--- YeastFunctions/Zymoliase/Matlab/script_image_analysis_AR_V1.m
+++ YeastFunctions/Zymoliase/Matlab/script_image_analysis_AR_V1.m
@@ -1,3 +1,5 @@
+% This script takes as input a folder with TIF raw images and applies the function specified in line 28 to each image in the folder.
+
 % Specify the folder where the files are.
 myFolder = 'C:\Users\uib\Nextcloud\LAB\Wetlab\alvaro_desktop\Experimentos\Experimentos homogeneizacion\Exp_ClusterSK1_shaker20hz_C1000_RT_20240305_tiff';
 % Check to make sure that folder actually exists. 
@@ -23,7 +25,7 @@ for k = 1 : length(theFiles)
     baseFileName = theFiles(k).name;
     fullFileName = fullfile(theFiles(k).folder, baseFileName);
     fprintf(1, 'Now reading %s\n', fullFileName);
-    image_analysis_agg_AR_V1(fullFileName, myFolder);
+    image_analysis_agg_AR_V1(fullFileName, myFolder);  %%% change the function as your desire.
      % Save the processed image with a new name (you can customize the name)
     [~, name, ext] = fileparts(baseFileName);
     saveFileName = fullfile(myFolder, [name, '_processed', ext]);
